ca8dbd11b014014ae72278c1f42d64f668c0e19e
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2018-04-02 18:07:53 +0200

    an artificial dataset, will be used in the documentation

---
+++ R/apartments.R
@@ -0,0 +1,22 @@
+#' Apartments Data
+#'
+#' Datasets \code{apartments} and \code{apartmentsTest} are artificial,
+#' generated form the same model.
+#' Structure of the dataset is copied from real dataset from \code{PBImisc} package,
+#' but they were generated in a way to mimic effect of Anscombe quartet for complex black box models.
+#'
+#' \itemize{
+#' \item m2.price - price per square meter
+#' \item surface - apartment area in square meters
+#' \item n.rooms - number of rooms (correlated with surface)
+#' \item district - district in which apartment is located, factor with 10 levels
+#' \item floor - floor
+#' \item construction.date - construction year
+#' }
+#'
+#' @docType data
+#' @keywords apartments
+#' @name apartments
+#' @usage data(apartments)
+#' @format a data frame with 1000 rows and 6 columns
+NULL
