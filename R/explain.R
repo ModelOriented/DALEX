71ea173dee5f8b8808f6a2bb523912410d5d31d0
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2018-09-29 12:41:30 -0700

    yhat function is now public, to avoid problems with scoping

--- R/explain.R
+++ R/explain.R
@@ -74,13 +74,31 @@ explain.default <- function(model, data = NULL, y = NULL, predict_function = yha
 #' @rdname explain
 explain <- explain.default
 
+#' Wrapper over the predict function
+#'
+#' This function is just a wrapper over the predict function.
+#' It sets different default parameters for models from different classes,
+#' like for classification random Forest is forces the output to be probabilities not classes itself.
+#'
+#' @param X.model object - a model to be explained
+#' @param newdata data.frame or matrix - observations for prediction
+#' @param ... other parameters that will be passed to the predict function
+#'
+#' @return An numeric matrix of predictions
+#'
+#' @rdname yhat
+#' @export
 yhat <- function (X.model, newdata, ...)
   UseMethod("yhat")
 
+#' @rdname yhat
+#' @export
 yhat.lm <- function(X.model, newdata, ...) {
   predict(X.model, newdata, ...)
 }
 
+#' @rdname yhat
+#' @export
 yhat.randomForest <- function(X.model, newdata, ...) {
   if (X.model$type == "classification") {
     predict(X.model, newdata, type = "prob", ...)
@@ -89,6 +107,8 @@ yhat.randomForest <- function(X.model, newdata, ...) {
   }
 }
 
+#' @rdname yhat
+#' @export
 yhat.default <- function(X.model, newdata, ...) {
   as.numeric(predict(X.model, newdata, ...))
 }
