15acba20d102843b27450f398328d46669e0cba2
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2019-04-03 16:51:03 +0200

    better startup message. Checks which suggested packages are missing

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,9 @@
 .onAttach <- function(...) {
+  addons <- setdiff(c("ALEPlot", "breakDown", "pdp", "factorMerger", "ggpubr"),
+                    rownames(installed.packages()))
+
   packageStartupMessage("Welcome to DALEX (version: ", utils::packageVersion("DALEX"), ").\n",
-        "This is a plain DALEX. Use 'install_dependencies()' to get all required packages.")
+    ifelse(length(addons) == 0, "",
+    paste0( "Additional features will be available after installation of: ",paste(addons, collapse = ", "),".\nUse 'install_dependencies()' to get all suggested dependencies"))
+        )
 }
