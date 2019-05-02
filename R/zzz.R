2bd75bb0555ab7de871921950991ab8f5a8144fe
Przemyslaw Biecek <przemyslaw.biecek@gmail.com>
2019-05-02 15:39:16 +0200

    installed.packages() should not be used in .onAttach
    quick.installed.packages() is faster than installed.packages

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,17 @@
+# installed.packages are slow, so let's find out what is installed quicker
+quick.installed.packages <- function() {
+  where_to_look <- .libPaths()
+  packages <- sapply(where_to_look, function(dir) list.dirs(dir, recursive = FALSE, full.names = FALSE))
+  unique(unlist(packages))
+}
+
 .onAttach <- function(...) {
-  addons <- setdiff(c("ALEPlot", "breakDown", "pdp", "factorMerger", "ggpubr"),
-                    rownames(installed.packages()))
+  addons <- setdiff(c("ingredients", "iBreakDown", "ALEPlot", "breakDown", "pdp", "factorMerger", "ggpubr"),
+                    quick.installed.packages())
 
   packageStartupMessage("Welcome to DALEX (version: ", utils::packageVersion("DALEX"), ").\n",
     ifelse(length(addons) == 0, "",
-    paste0( "Additional features will be available after installation of: ",paste(addons, collapse = ", "),".\nUse 'install_dependencies()' to get all suggested dependencies"))
+    paste0( "Additional features will be available after installation of: ",
+            paste(addons, collapse = ", "),".\nUse 'install_dependencies()' to get all suggested dependencies"))
         )
 }
