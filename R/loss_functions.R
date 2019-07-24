1e813b4a51e2312b487d7db4505916b6566d0764
maksymiuks <32574056+maksymiuks@users.noreply.github.com>
2019-07-24 17:30:03 +0200

    ver 0.4.6 update

--- R/loss_functions.R
+++ R/loss_functions.R
@@ -7,7 +7,7 @@
 #'
 #' @return numeric - value of the loss function
 #'
-#' @aliases loss_cross_entropy loss_sum_of_squares loss_root_mean_square loss_accuracy
+#' @aliases loss_cross_entropy loss_sum_of_squares loss_root_mean_square loss_accuracy loss_one_auc
 #' @export
 #' @examples
 #'  \dontrun{
@@ -16,16 +16,34 @@
 #' loss_sum_of_squares(as.numeric(HR$status == "fired"), yhat(HR_rf_model))
 #'  }
 #' @export
-loss_cross_entropy = function(observed, predicted, p_min = 0.0001, na.rm = TRUE) {
+loss_cross_entropy <- function(observed, predicted, p_min = 0.0001, na.rm = TRUE) {
   p <- sapply(seq_along(observed), function(i)  predicted[i, observed[i]] )
   sum(-log(pmax(p, p_min)), na.rm = TRUE)
 }
 
 #' @export
-loss_sum_of_squares = function(observed, predicted, na.rm = TRUE) sum((observed - predicted)^2, na.rm = na.rm)
+loss_sum_of_squares <- function(observed, predicted, na.rm = TRUE) sum((observed - predicted)^2, na.rm = na.rm)
 
 #' @export
-loss_root_mean_square = function(observed, predicted, na.rm = TRUE) sqrt(mean((observed - predicted)^2, na.rm = na.rm))
+loss_root_mean_square <- function(observed, predicted, na.rm = TRUE) sqrt(mean((observed - predicted)^2, na.rm = na.rm))
 
 #' @export
-loss_accuracy = function(observed, predicted, na.rm = TRUE) mean(observed == predicted, na.rm = na.rm)
+loss_accuracy <-  function(observed, predicted, na.rm = TRUE) mean(observed == predicted, na.rm = na.rm)
+
+#' @export
+# Alicja Gosiewska (agosiewska) is the author of this function
+loss_one_auc <- function(observed, predicted){
+
+  pred <- data.frame(fitted.values = predicted,
+             y = observed)
+  pred_sorted <- pred[order(pred$fitted.values, decreasing = TRUE), ]
+  roc_y <- factor(pred_sorted$y)
+  levels <- levels(roc_y)
+  x = cumsum(roc_y == levels[1])/sum(roc_y == levels[1])
+  y = cumsum(roc_y == levels[2])/sum(roc_y == levels[2])
+  auc = sum((x[2:length(roc_y)]-x[1:length(roc_y)-1])*y[2:length(roc_y)])
+  1 - auc
+
+}
+
+
