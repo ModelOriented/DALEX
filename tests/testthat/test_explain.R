6df80f42460a5994914b362c77218aaa2fa56cab
maksymiuks <32574056+maksymiuks@users.noreply.github.com>
2019-08-21 14:27:51 +0200

    New test + names changed to english

--- tests/testthat/test_explain.R
+++ tests/testthat/test_explain.R
@@ -2,9 +2,11 @@ context("Check explain() function")
 
 test_that("Type of data in the explainer",{
   apartmentsTest_tibble <- dplyr::as_tibble(apartments_test)
-  model_liniowy <- lm(m2.price ~ construction.year + surface + floor + no.rooms + district, data = apartments)
+  linear_model <- lm(m2.price ~ construction.year + surface + floor + no.rooms + district, data = apartments)
 
-  explainer_lm <- DALEX::explain(model_liniowy, data = apartmentsTest_tibble[,2:6], y = apartmentsTest_tibble$m2.price)
+  explainer_lm1 <- DALEX::explain(linear_model, data = apartmentsTest_tibble[,2:6], y = apartmentsTest_tibble$m2.price)
+  explainer_lm2 <- DALEX::explain(linear_model, data = apartmentsTest_tibble[,2:6])
 
-  expect_true(is.data.frame(explainer_lm$data))
+  expect_true(is.data.frame(explainer_lm1$data))
+  expect_is(explainer_lm2, "explainer")
 })
