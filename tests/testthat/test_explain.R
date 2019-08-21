45665114c2fb7c76b98a4781cf2a6de66d112339
maksymiuks <32574056+maksymiuks@users.noreply.github.com>
2019-08-21 22:41:31 +0200

    Optimalization of model_performance and tests for it

--- tests/testthat/test_explain.R
+++ tests/testthat/test_explain.R
@@ -1,11 +1,10 @@
 context("Check explain() function")
 
 test_that("Type of data in the explainer",{
-  apartmentsTest_tibble <- dplyr::as_tibble(apartments_test)
   linear_model <- lm(m2.price ~ construction.year + surface + floor + no.rooms + district, data = apartments)
 
-  explainer_lm1 <- DALEX::explain(linear_model, data = apartmentsTest_tibble[,2:6], y = apartmentsTest_tibble$m2.price)
-  explainer_lm2 <- DALEX::explain(linear_model, data = apartmentsTest_tibble[,2:6])
+  explainer_lm1 <- DALEX::explain(linear_model, data = apartments[,2:6], y = apartments$m2.price)
+  explainer_lm2 <- DALEX::explain(linear_model, data = apartments[,2:6])
 
   expect_true(is.data.frame(explainer_lm1$data))
   expect_is(explainer_lm2, "explainer")
