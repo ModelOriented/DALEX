a0de354627f631706828315eae2f7e6cc10b5d2e
Przemyslaw Biecek <przemyslaw.biecek@gmail.com>
2019-08-25 11:42:34 +0200

    fixed default predict function in tests

--- tests/testthat/test_explain.R
+++ tests/testthat/test_explain.R
@@ -5,10 +5,10 @@ test_that("Type of data in the explainer",{
 
   explainer_lm1 <- explain(linear_model)
   explainer_lm2 <- explain(linear_model, verbose = FALSE)
-  explainer_lm3 <- explain(linear_model, data = apartments, label = "model_4v", predict_function = predict)
+  explainer_lm3 <- explain(linear_model, data = apartments, label = "model_4v", predict_function = DALEX::yhat.randomForest)
   explainer_lm4 <- explain(linear_model, data = apartments, label = "model_4v", y = apartments$m2.price)
   explainer_lm5 <- explain(linear_model, data = apartments, label = "model_4v", y = apartments$m2.price,
-                    predict_function = predict)
+                           predict_function = DALEX::yhat.randomForest)
 
   expect_true(is.data.frame(explainer_lm1$data))
   expect_is(explainer_lm1, "explainer")
