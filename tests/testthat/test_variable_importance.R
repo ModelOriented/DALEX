f88858db9e62b70b2c429ed7e949783a3415c298
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2020-01-03 02:29:33 +0100

    test for desc_sorting is moved to ingredients

--- tests/testthat/test_variable_importance.R
+++ tests/testthat/test_variable_importance.R
@@ -22,8 +22,6 @@ test_that("Output format - plot",{
 test_that("Inverse sorting of bars",{
   expect_is(plot(vi_rf_ratio, desc_sorting = FALSE), "gg")
   expect_is(plot(vi_rf, vi_lm, desc_sorting = FALSE), "gg")
-  expect_error(plot(vi_rf_ratio, desc_sorting = "desc"))
-  expect_error(plot(vi_rf, vi_lm, desc_sorting = "desc"))
 })
 
 
