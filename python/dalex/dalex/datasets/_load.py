7ca47c83b3820320d4a78b1df5a0bbc9a80dacb5
hbaniecki <hbaniecki@gmail.com>
2020-04-19 01:07:43 +0200

    add fifa

--- python/dalex/dalex/datasets/_load.py
+++ python/dalex/dalex/datasets/_load.py
@@ -19,11 +19,13 @@ def load_fifa():
     """
     Load the preprocessed players_20 dataset.
     It contains 5000 'overall' best players and 43 columns. These are:
-    - name and nationality of the player (not used in modeling)
+    - short_name and nationality of the player (not used in modeling)
     - overall, potential, value_eur, wage_eur (4 potential target variables)
     - age, height, weight, attacking skills, defending skills, goalkeeping skills (37 variables)
 
-    Link: https://www.kaggle.com/stefanoleone992/fifa-20-complete-player-dataset
+    It is advised to leave only one target variable for modeling.
+
+    Source: https://www.kaggle.com/stefanoleone992/fifa-20-complete-player-dataset January 1, 2020
     """
 
     abs_dir_path = os.path.dirname(os.path.abspath(__file__))
