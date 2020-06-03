11963766e8359d43fb301ce97431046bdaf818da
Wojciech Kretowicz <32495076+WojciechKretowicz@users.noreply.github.com>
2020-06-03 16:02:15 +0200

    update warning

--- python/dalex/dalex/instance_level/_shap/checks.py
+++ python/dalex/dalex/instance_level/_shap/checks.py
@@ -49,7 +49,7 @@ def check_new_observation(new_observation, explainer):
 def check_processes(processes):
     from multiprocessing import cpu_count
     if processes > cpu_count():
-        warn("You have provided too many processes, truncated you maximum.")
+        warn("You have provided too many processes, truncated to the number of physical CPUs.")
 
         return cpu_count()
 
