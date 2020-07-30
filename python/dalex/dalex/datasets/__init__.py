865d47e8a0a118814006dad4b960c73a9d179d97
Hubert Baniecki <hbaniecki@gmail.com>
2020-07-30 20:21:06 +0200

    fix dataset import

--- python/dalex/dalex/datasets/__init__.py
+++ python/dalex/dalex/datasets/__init__.py
@@ -2,5 +2,11 @@ from ._load import load_titanic, load_fifa
 
 __all__ = [
     "load_titanic",
-    "load_fifa"
+    "load_fifa",
+    "load_apartments",
+    "load_apartemnts_test",
+    "load_dragons",
+    "load_dragons_test",
+    "load_hr",
+    "load_hr_test"
 ]
