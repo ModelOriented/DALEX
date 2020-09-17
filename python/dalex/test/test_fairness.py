48bd5b59d909fd394687731131b2b1a3d73fb5c2
jakwisn <jakwisn@gmail.com>
2020-09-17 20:57:51 +0200

    fairness tests

--- python/dalex/test/test_fairness.py
+++ python/dalex/test/test_fairness.py
@@ -1,17 +1,17 @@
 import unittest
 import numpy as np
-
+from dalex.fairness.group_fairness.utils import *
 
 
 
 class FairnessTest(unittest.TestCase):
 
     def test_ConfusionMatrix(self):
-        from dalex.fairness.group_fairness.utils import _ConfusionMatrix
+        from dalex.fairness.group_fairness.utils import ConfusionMatrix
         y_true = np.array([0, 0, 0, 0, 1, 1, 1, 1])
         y_pred = np.array([0.32, 0.54, 0.56, 0.67, 0.34, 0.67, 0.98, 1])
         cutoff = 0.55
-        cm = _ConfusionMatrix(y_true, y_pred, cutoff)
+        cm = ConfusionMatrix(y_true, y_pred, cutoff)
 
         #  proper calculations
         self.assertEqual(cm.cutoff, 0.55)
@@ -23,11 +23,11 @@ class FairnessTest(unittest.TestCase):
         #  error assertions
         y_true = y_true[:-1]
         with self.assertRaises(AssertionError):
-            cm_ = _ConfusionMatrix(y_true, y_pred, cutoff)
+            cm_ = ConfusionMatrix(y_true, y_pred, cutoff)
         y_true = np.append(y_true, 1)
         cutoff = 1.5
         with self.assertRaises(AssertionError):
-            cm_ = _ConfusionMatrix(y_true, y_pred, cutoff)
+            cm_ = ConfusionMatrix(y_true, y_pred, cutoff)
 
     protected = np.array(['a', 'a', 'a', 'a', 'a', 'a', 'b', 'b', 'b', 'c', 'c', 'c', 'c'])
     y_true = np.array([0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0])
@@ -36,14 +36,14 @@ class FairnessTest(unittest.TestCase):
 
 
     def test_SubConfusionMatrix(self):
-        from dalex.fairness.group_fairness.utils import _SubConfusionMatrix
+        from dalex.fairness.group_fairness.utils import SubgroupConfusionMatrix
 
         y_true = FairnessTest.y_true
         y_pred = FairnessTest.y_pred
         protected = FairnessTest.protected
         cutoff = FairnessTest.cutoff
 
-        scf = _SubConfusionMatrix(y_true, y_pred, protected, cutoff)
+        scf = SubgroupConfusionMatrix(y_true, y_pred, protected, cutoff)
 
         #  proper calculations
         self.assertEqual(scf.sub_dict.get('a').tp, 2)
@@ -64,30 +64,78 @@ class FairnessTest(unittest.TestCase):
         #  error assertions
         y_true = y_true[:-1]
         with self.assertRaises(AssertionError):
-            cm_ = _SubConfusionMatrix(y_true, y_pred, protected,  cutoff)
+            cm_ = SubgroupConfusionMatrix(y_true, y_pred, protected, cutoff)
         y_true = np.append(y_true, 1)
         cutoff = [0.1, 0.2, 0.4]  # list instead of dict
         with self.assertRaises(AssertionError):
-            cm_ = _SubConfusionMatrix(y_true, y_pred, protected,  cutoff)
+            cm_ = SubgroupConfusionMatrix(y_true, y_pred, protected, cutoff)
 
     def test_SubgroupConfusionMatrixMetrics(self):
-        from dalex.fairness.group_fairness.utils import _SubConfusionMatrix
-        from dalex.fairness.group_fairness.utils import _SubroupConfusionMatrixMetrics
 
         y_true = FairnessTest.y_true
         y_pred = FairnessTest.y_pred
         protected = FairnessTest.protected
         cutoff = FairnessTest.cutoff
 
-        scf = _SubConfusionMatrix(y_true, y_pred, protected, cutoff)
-        scf_metrics = _SubroupConfusionMatrixMetrics(scf)
+        scf = SubgroupConfusionMatrix(y_true, y_pred, protected, cutoff)
+        scf_metrics = SubgroupConfusionMatrixMetrics(scf)
+        scmm = scf_metrics.subgroup_confusion_matrix_metrics
+        self.assertEqual(scmm.get('a').get('TPR'), 1)
+        self.assertEqual(scmm.get('b').get('TPR'), 0.667)
+        self.assertTrue(np.isnan(scmm.get('b').get('TNR')))
 
-        # @TODO self.assertEqual()
+    def test_calculate_ratio(self):
 
-    def test_metrics(self):
-        # @TODO
+
+        y_true = FairnessTest.y_true
+        y_pred = FairnessTest.y_pred
+        protected = FairnessTest.protected
+        cutoff = FairnessTest.cutoff
+
+        scf = SubgroupConfusionMatrix(y_true, y_pred, protected, cutoff)
+        scf_metrics = SubgroupConfusionMatrixMetrics(scf)
+
+        df_ratio = calculate_ratio(scf_metrics, 'a')
+
+        b = list(scf_metrics.subgroup_confusion_matrix_metrics.get('b').values())
+        a = list(scf_metrics.subgroup_confusion_matrix_metrics.get('a').values())
+
+        ratio = np.array(b)/np.array(a)
+        ratio[np.isinf(ratio)] = np.nan
+        ratio[ratio == 0] = np.nan
+
+        ratio_nonnan = ratio[np.isfinite(ratio)]
+        df_ratio_nonnan = np.array(df_ratio.iloc[1,:][np.isfinite(df_ratio.iloc[1,:])])
+
+        self.assertTrue(np.equal(ratio_nonnan, df_ratio_nonnan).all())
+
+    def test_calculate_parity_loss(self):
+
+        y_true = FairnessTest.y_true
+        y_pred = FairnessTest.y_pred
+        protected = FairnessTest.protected
+        cutoff = FairnessTest.cutoff
+
+        scf = SubgroupConfusionMatrix(y_true, y_pred, protected, cutoff)
+        scf_metrics = SubgroupConfusionMatrixMetrics(scf)
+
+        parity_loss = calculate_parity_loss(scf_metrics, "a")
+        ratios = calculate_ratio(scf_metrics, "a")
+        TPR_parity_loss = parity_loss.iloc[0]
+
+        TPR_ratios = ratios.TPR/ratios.TPR[0]
+        TPR_log = np.log(TPR_ratios)
+
+        self.assertEqual(TPR_log.sum(), TPR_parity_loss)
+
+
+    def test_GroupFairnessObject(self):
+        # TODO
         pass
 
+    def test_model_group_fairness(self):
+        # TODO
+        pass
 
 if __name__ == '__main__':
     unittest.main()
