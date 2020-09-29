3fc3ab811b9c3f039e93331228f73c8f0ad67545
jakwisn <jakwisn@gmail.com>
2020-09-29 10:34:16 +0200

    added support for pandas

--- python/dalex/dalex/fairness/basics/checks.py
+++ python/dalex/dalex/fairness/basics/checks.py
@@ -5,7 +5,6 @@ import pandas as pd
 
 
 def check_parameters(y, y_hat, protected, privileged, verbose):
-    # @TODO crashes when pd.series is provided instead of np.array in protected
     if not len(y) == len(y_hat) == len(protected):
         raise ParameterCheckError("protected and explainer attributes y, y_hat must have the same lengths")
 
@@ -18,29 +17,36 @@ def check_parameters(y, y_hat, protected, privileged, verbose):
     if not 0 <= y_hat.all() <= 1:
         raise ParameterCheckError("y_hat must have probabilistic output between 0 and 1")
 
-    if isinstance(protected, list):
-        try:
-            verbose_cat("protected list will be converted to nd.array")
-            protected = np.array(protected)
-        except:
-            ParameterCheckError("failed to convert list to nd.array")
-    elif isinstance(protected, pd.Series):
+    if not isinstance(protected, np.ndarray):
+    # if is not numpy array check what type it is and change to np array
+        if isinstance(protected, list):
+            try:
+                verbose_cat("protected list will be converted to nd.array", verbose)
+                protected = np.array(protected, dtype = 'U')
+            except:
+                ParameterCheckError("failed to convert list to nd.array", verbose)
 
-        try:
-            verbose_cat("protected Series will be converted to nd.array")
-            protected = protected.to_numpy()
-        except:
-            ParameterCheckError("failed to convert list to nd.array")
+        elif isinstance(protected, pd.Series):
+            try:
+                verbose_cat("protected Series will be converted to nd.array", verbose)
+                protected = np.array(protected, dtype = 'U')
+            except:
+                ParameterCheckError("failed to convert list to nd.array")
+        else:
+            ParameterCheckError("Unsupported protected type provided. Please convert protected to flat np.ndarray")
 
-    else:
-        ParameterCheckError("Please convert protected to flat np.ndarray")
-
-    if protected.dtype.type is not np.string_:
+    if protected.dtype.type is not np.str_:
         verbose_cat("protected array is not string type, converting to string ", verbose)
         try:
             protected = protected.astype(str)
         except:
             ParameterCheckError('Could not convert protected to String type')
+
+    if isinstance(privileged, int):
+        try:
+            privileged = str(privileged)
+        except:
+            ParameterCheckError('Could not convert privileged to String')
 
     if not isinstance(privileged, str):
         raise ParameterCheckError('privileged parameter must be a String')
