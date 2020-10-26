02444a59c321475fed7b22708a8763dc1cca2d71
hbaniecki <hbaniecki@gmail.com>
2020-10-26 21:37:37 +0100

    update fairness tests

--- python/dalex/test/test_fairness.py
+++ python/dalex/test/test_fairness.py
@@ -1,22 +1,49 @@
 import unittest
-from dalex.fairness._group_fairness.utils import *
-from dalex.fairness._group_fairness.object import GroupFairnessClassification
-from dalex.fairness._basics.exceptions import *
+
+from copy import deepcopy
+import numpy as np
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.tree import DecisionTreeClassifier
+
 import dalex as dx
+from plotly.graph_objs import Figure
 
 
 class FairnessTest(unittest.TestCase):
 
+    def setUp(self):
+        self.protected = np.array(['a', 'a', 'a', 'a', 'a', 'a', 'b', 'b', 'b', 'c', 'c', 'c', 'c'])
+        self.y_true = np.array([0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0])
+        self.y_pred = np.array([0.32, 0.43, 0.56, 0.67, 0.9, 0.67, 0.98, 0.1, 0.44, 1, 0.65, 0.55, 1])
+        self.cutoff = {'a': 0.5, 'b': 0.4, 'c': 0.6}
+
+        data = dx.datasets.load_german()
+
+        X = data.drop(columns='risk')
+        y = data.risk
+
+        categorical_features = ['sex', 'job', 'housing', 'saving_accounts', "checking_account", 'purpose']
+        categorical_transformer = Pipeline(steps=[
+            ('onehot', OneHotEncoder(handle_unknown='ignore'))])
+
+        preprocessor = ColumnTransformer(
+            transformers=[
+                ('cat', categorical_transformer, categorical_features)])
+
+        clf = Pipeline(steps=[('preprocessor', preprocessor),
+                              ('classifier', DecisionTreeClassifier(max_depth=7, random_state=123))])
+        clf.fit(X, y)
+
+        self.exp = dx.Explainer(clf, X, y, verbose=False)
+        self.german_protected = data.sex + '_' + np.where(data.age < 25, 'young', 'old')
+
     def test_ConfusionMatrix(self):
-        from dalex.fairness._group_fairness.utils import ConfusionMatrix
         y_true = np.array([0, 0, 0, 0, 1, 1, 1, 1])
         y_pred = np.array([0.32, 0.54, 0.56, 0.67, 0.34, 0.67, 0.98, 1])
         cutoff = 0.55
-        cm = ConfusionMatrix(y_true, y_pred, cutoff)
+        cm = dx.fairness._group_fairness.utils.ConfusionMatrix(y_true, y_pred, cutoff)
 
         #  proper calculations
         self.assertEqual(cm.cutoff, 0.55)
@@ -28,26 +55,20 @@ class FairnessTest(unittest.TestCase):
         #  error assertions
         y_true = y_true[:-1]
         with self.assertRaises(AssertionError):
-            cm_ = ConfusionMatrix(y_true, y_pred, cutoff)
+            cm_ = dx.fairness._group_fairness.utils.ConfusionMatrix(y_true, y_pred, cutoff)
         y_true = np.append(y_true, 1)
         cutoff = 1.5
         with self.assertRaises(AssertionError):
-            cm_ = ConfusionMatrix(y_true, y_pred, cutoff)
-
-    protected = np.array(['a', 'a', 'a', 'a', 'a', 'a', 'b', 'b', 'b', 'c', 'c', 'c', 'c'])
-    y_true = np.array([0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0])
-    y_pred = np.array([0.32, 0.43, 0.56, 0.67, 0.9, 0.67, 0.98, 0.1, 0.44, 1, 0.65, 0.55, 1])
-    cutoff = {'a': 0.5, 'b': 0.4, 'c': 0.6}
+            cm_ = dx.fairness._group_fairness.utils.ConfusionMatrix(y_true, y_pred, cutoff)
 
     def test_SubConfusionMatrix(self):
-        from dalex.fairness._group_fairness.utils import SubgroupConfusionMatrix
 
-        y_true = FairnessTest.y_true
-        y_pred = FairnessTest.y_pred
-        protected = FairnessTest.protected
-        cutoff = FairnessTest.cutoff
+        y_true = self.y_true.copy()
+        y_pred = self.y_pred
+        protected = self.protected
+        cutoff = self.cutoff
 
-        scf = SubgroupConfusionMatrix(y_true, y_pred, protected, cutoff)
+        scf = dx.fairness._group_fairness.utils.SubgroupConfusionMatrix(y_true, y_pred, protected, cutoff)
 
         #  proper calculations
         self.assertEqual(scf.sub_dict.get('a').tp, 2)
@@ -68,35 +89,35 @@ class FairnessTest(unittest.TestCase):
         #  error assertions
         y_true = y_true[:-1]
         with self.assertRaises(AssertionError):
-            cm_ = SubgroupConfusionMatrix(y_true, y_pred, protected, cutoff)
+            cm_ = dx.fairness._group_fairness.utils.SubgroupConfusionMatrix(y_true, y_pred, protected, cutoff)
         y_true = np.append(y_true, 1)
         cutoff = [0.1, 0.2, 0.4]  # list instead of dict
         with self.assertRaises(AssertionError):
-            cm_ = SubgroupConfusionMatrix(y_true, y_pred, protected, cutoff)
+            cm_ = dx.fairness._group_fairness.utils.SubgroupConfusionMatrix(y_true, y_pred, protected, cutoff)
 
     def test_SubgroupConfusionMatrixMetrics(self):
-        y_true = FairnessTest.y_true
-        y_pred = FairnessTest.y_pred
-        protected = FairnessTest.protected
-        cutoff = FairnessTest.cutoff
+        y_true = self.y_true
+        y_pred = self.y_pred
+        protected = self.protected
+        cutoff = self.cutoff
 
-        scf = SubgroupConfusionMatrix(y_true, y_pred, protected, cutoff)
-        scf_metrics = SubgroupConfusionMatrixMetrics(scf)
+        scf = dx.fairness._group_fairness.utils.SubgroupConfusionMatrix(y_true, y_pred, protected, cutoff)
+        scf_metrics = dx.fairness._group_fairness.utils.SubgroupConfusionMatrixMetrics(scf)
         scmm = scf_metrics.subgroup_confusion_matrix_metrics
         self.assertEqual(scmm.get('a').get('TPR'), 1)
         self.assertEqual(scmm.get('b').get('TPR'), 0.667)
         self.assertTrue(np.isnan(scmm.get('b').get('TNR')))
 
     def test_calculate_ratio(self):
-        y_true = FairnessTest.y_true
-        y_pred = FairnessTest.y_pred
-        protected = FairnessTest.protected
-        cutoff = FairnessTest.cutoff
+        y_true = self.y_true
+        y_pred = self.y_pred
+        protected = self.protected
+        cutoff = self.cutoff
 
-        scf = SubgroupConfusionMatrix(y_true, y_pred, protected, cutoff)
-        scf_metrics = SubgroupConfusionMatrixMetrics(scf)
+        scf = dx.fairness._group_fairness.utils.SubgroupConfusionMatrix(y_true, y_pred, protected, cutoff)
+        scf_metrics = dx.fairness._group_fairness.utils.SubgroupConfusionMatrixMetrics(scf)
 
-        df_ratio = calculate_ratio(scf_metrics, 'a')
+        df_ratio = dx.fairness._group_fairness.utils.calculate_ratio(scf_metrics, 'a')
 
         b = list(scf_metrics.subgroup_confusion_matrix_metrics.get('b').values())
         a = list(scf_metrics.subgroup_confusion_matrix_metrics.get('a').values())
@@ -111,92 +132,72 @@ class FairnessTest(unittest.TestCase):
         self.assertTrue(np.equal(ratio_nonnan, df_ratio_nonnan).all())
 
     def test_calculate_parity_loss(self):
-        y_true = FairnessTest.y_true
-        y_pred = FairnessTest.y_pred
-        protected = FairnessTest.protected
-        cutoff = FairnessTest.cutoff
+        y_true = self.y_true
+        y_pred = self.y_pred
+        protected = self.protected
+        cutoff = self.cutoff
 
-        scf = SubgroupConfusionMatrix(y_true, y_pred, protected, cutoff)
-        scf_metrics = SubgroupConfusionMatrixMetrics(scf)
+        scf = dx.fairness._group_fairness.utils.SubgroupConfusionMatrix(y_true, y_pred, protected, cutoff)
+        scf_metrics = dx.fairness._group_fairness.utils.SubgroupConfusionMatrixMetrics(scf)
 
-        parity_loss = calculate_parity_loss(scf_metrics, "a")
-        ratios = calculate_ratio(scf_metrics, "a")
+        parity_loss = dx.fairness._group_fairness.utils.calculate_parity_loss(scf_metrics, "a")
+        ratios = dx.fairness._group_fairness.utils.calculate_ratio(scf_metrics, "a")
         TPR_parity_loss = parity_loss.iloc[0]
 
         TPR_ratios = ratios.TPR / ratios.TPR[0]
-        TPR_log =abs(np.log(TPR_ratios))
+        TPR_log = abs(np.log(TPR_ratios))
 
         self.assertEqual(TPR_log.sum(), TPR_parity_loss)
 
-    data = dx.datasets.load_german()
+    def test_GroupFairnessClassification(self):
+        exp = self.exp
+        protected = self.german_protected
 
-    X = data.drop(columns='risk')
-    y = data.risk
-
-    categorical_features = ['sex', 'job', 'housing', 'saving_accounts', "checking_account", 'purpose']
-    categorical_transformer = Pipeline(steps=[
-        ('onehot', OneHotEncoder(handle_unknown='ignore'))])
-
-    preprocessor = ColumnTransformer(
-        transformers=[
-            ('cat', categorical_transformer, categorical_features)])
-
-    clf = Pipeline(steps=[('preprocessor', preprocessor),
-                          ('classifier', DecisionTreeClassifier(max_depth=7, random_state=123))])
-    clf.fit(X, y)
-
-    exp = dx.Explainer(clf, X, y)
-    german_protected = data.sex + '_' + np.where(data.age < 25, 'young', 'old')
-
-    def test_GroupFairnessClassificationObject(self):
-        exp = FairnessTest.exp
-        protected = FairnessTest.german_protected
-
-        gfco = GroupFairnessClassification(y=exp.y,
-                                           y_hat=exp.y_hat,
-                                           protected=protected,
-                                           privileged='male_old',
-                                           verbose=False,
-                                           label=exp.label)
-        self.assertEqual(gfco.__class__.__name__, 'GroupFairnessClassification')
+        gfco = dx.fairness.GroupFairnessClassification(y=exp.y,
+                                                       y_hat=exp.y_hat,
+                                                       protected=protected,
+                                                       privileged='male_old',
+                                                       verbose=False,
+                                                       label=exp.label)
+        self.assertIsInstance(gfco, dx.fairness.GroupFairnessClassification)
 
     def test_parameter_checks(self):
-        exp = FairnessTest.exp
-        protected = FairnessTest.german_protected
+        exp = self.exp
+        protected = self.german_protected
 
         #  error handling
         wrong_protected = np.array([protected, protected])
-        with self.assertRaises(ParameterCheckError):
+        with self.assertRaises(dx.fairness._basics.exceptions.ParameterCheckError):
             gfco = exp.model_fairness(protected=wrong_protected,
                                       privileged='male_old',
                                       verbose=False)
 
-        with self.assertRaises(ParameterCheckError):
+        with self.assertRaises(dx.fairness._basics.exceptions.ParameterCheckError):
             gfco = exp.model_fairness(protected=protected,
                                       privileged='not_existing',
                                       verbose=False)
-        with self.assertRaises(ParameterCheckError):
-            gfco = GroupFairnessClassification(y=exp.y[:-1, ],
-                                               y_hat=exp.y_hat,
-                                               protected=protected,
-                                               privileged='male_old',
-                                               verbose=False,
-                                               label=exp.label)
-        with self.assertRaises(ParameterCheckError):
-            gfco = GroupFairnessClassification(y=exp.y[:-1, ],
-                                               y_hat=exp.y_hat[:-1, ],
-                                               protected=protected,
-                                               privileged='male_old',
-                                               verbose=False,
-                                               label=exp.label)
+        with self.assertRaises(dx.fairness._basics.exceptions.ParameterCheckError):
+            gfco = dx.fairness.GroupFairnessClassification(y=exp.y[:-1, ],
+                                                           y_hat=exp.y_hat,
+                                                           protected=protected,
+                                                           privileged='male_old',
+                                                           verbose=False,
+                                                           label=exp.label)
+        with self.assertRaises(dx.fairness._basics.exceptions.ParameterCheckError):
+            gfco = dx.fairness.GroupFairnessClassification(y=exp.y[:-1, ],
+                                                           y_hat=exp.y_hat[:-1, ],
+                                                           protected=protected,
+                                                           privileged='male_old',
+                                                           verbose=False,
+                                                           label=exp.label)
 
-        with self.assertRaises(ParameterCheckError):
+        with self.assertRaises(dx.fairness._basics.exceptions.ParameterCheckError):
             gfco = exp.model_fairness(protected=protected,
                                       privileged='male_old',
                                       cutoff=1.2,
                                       verbose=False)
 
-        with self.assertRaises(ParameterCheckError):
+        with self.assertRaises(dx.fairness._basics.exceptions.ParameterCheckError):
             gfco = exp.model_fairness(protected=protected,
                                       privileged='male_old',
                                       cutoff='not_int',
@@ -230,46 +231,46 @@ class FairnessTest(unittest.TestCase):
                                   verbose=False)
 
         self.assertEqual(type(gfco.protected), np.ndarray)
+        self.assertIsInstance(gfco, dx.fairness.GroupFairnessClassification)
 
     def test_model_group_fairness(self):
-        exp = FairnessTest.exp
-        protected = FairnessTest.german_protected
+        exp = self.exp
+        protected = self.german_protected
         mgf = exp.model_fairness(protected=protected,
                                  privileged='male_old',
                                  verbose=False)
 
-        self.assertEqual(mgf.__class__.__name__, 'GroupFairnessClassification')
+        self.assertIsInstance(mgf, dx.fairness.GroupFairnessClassification)
 
     def test_plot_fairness_check(self):
-        import plotly.graph_objects as go
-        exp = FairnessTest.exp
-        protected = FairnessTest.german_protected
+        exp = self.exp
+        protected = self.german_protected
         mgf = exp.model_fairness(protected=protected,
                                  privileged='male_old',
                                  verbose=False)
-        fig = mgf.plot(show=False)
-        self.assertEqual(fig.layout.title.text, "Fairness Check")
-        self.assertEqual(fig.__class__, go.Figure)
+        fig1 = mgf.plot(show=False)
+        self.assertEqual(fig1.layout.title.text, "Fairness Check")
+        self.assertIsInstance(fig1, Figure)
 
         mgf2 = deepcopy(mgf)
         mgf.label = 'first'
         mgf2.label = 'second'
 
-        fig = mgf.plot(objects=[mgf2], show=False)
-        self.assertEqual(fig.__class__, go.Figure)
+        fig2 = mgf.plot(objects=[mgf2], show=False)
+        self.assertIsInstance(fig2, Figure)
 
-        self.assertEqual(fig['data'][0]['legendgroup'], "first")
-        self.assertEqual(fig['data'][5]['legendgroup'], "second")
+        self.assertEqual(fig2['data'][0]['legendgroup'], "first")
+        self.assertEqual(fig2['data'][5]['legendgroup'], "second")
 
         # test errors in plots
-        with self.assertRaises(FairnessObjectsDifferenceError):
+        with self.assertRaises(dx.fairness._basics.exceptions.FairnessObjectsDifferenceError):
             mgf_wrong = exp.model_fairness(protected=protected,
                                            privileged='male_young',
                                            verbose=False
                                            )
             mgf.plot([mgf_wrong])
 
-        with self.assertRaises(FairnessObjectsDifferenceError):
+        with self.assertRaises(dx.fairness._basics.exceptions.FairnessObjectsDifferenceError):
             exp_wrong = deepcopy(exp)
             exp_wrong.y = exp_wrong.y[:-1]
             exp_wrong.y_hat = exp_wrong.y_hat[:-1]
@@ -280,35 +281,33 @@ class FairnessTest(unittest.TestCase):
             mgf.plot([mgf_wrong])
 
     def test_plot_metric_scores(self):
-        import plotly.graph_objects as go
-        exp = FairnessTest.exp
-        protected = FairnessTest.german_protected
+        exp = self.exp
+        protected = self.german_protected
         mgf = exp.model_fairness(protected=protected,
                                  privileged='male_old',
                                  verbose=False)
-        fig = mgf.plot(show=False, type='metric_scores')
-        self.assertEqual(fig.layout.title.text, "Metric Scores")
-        self.assertEqual(fig.__class__, go.Figure)
+        fig1 = mgf.plot(show=False, type='metric_scores')
+        self.assertEqual(fig1.layout.title.text, "Metric Scores")
+        self.assertIsInstance(fig1, Figure)
 
         mgf2 = deepcopy(mgf)
         mgf.label = 'first'
         mgf2.label = 'second'
 
-        fig = mgf.plot(objects=[mgf2], show=False, type='metric_scores')
-        self.assertEqual(fig.__class__, go.Figure)
+        fig2 = mgf.plot(objects=[mgf2], show=False, type='metric_scores')
+        self.assertIsInstance(fig2, Figure)
 
-        self.assertEqual(fig['data'][0]['legendgroup'], "first")
-        self.assertEqual(fig['data'][5]['legendgroup'], "second")
+        self.assertEqual(fig2['data'][0]['legendgroup'], "first")
+        self.assertEqual(fig2['data'][5]['legendgroup'], "second")
 
         # test errors in plots
-        with self.assertRaises(FairnessObjectsDifferenceError):
+        with self.assertRaises(dx.fairness._basics.exceptions.FairnessObjectsDifferenceError):
             mgf_wrong = exp.model_fairness(protected=protected,
                                            privileged='male_young',
-                                           verbose=False
-                                           )
+                                           verbose=False)
             mgf.plot([mgf_wrong], type='metric_scores')
 
-        with self.assertRaises(FairnessObjectsDifferenceError):
+        with self.assertRaises(dx.fairness._basics.exceptions.FairnessObjectsDifferenceError):
             exp_wrong = deepcopy(exp)
             exp_wrong.y = exp_wrong.y[:-1]
             exp_wrong.y_hat = exp_wrong.y_hat[:-1]
