92e6ed65d5d850143281263c87f19536b1453610
Piotr Piątyszek <piotrp@wektor.xyz>
2021-03-06 22:39:42 +0000

    Add dependence and varaiable importance charts based on shapley values

---
+++ python/dalex/dalex/arena/plots/_shapley_values_dependence_container.py
@@ -0,0 +1,44 @@
+from pandas.api.types import is_numeric_dtype
+from .._plot_container import PlotContainer
+
+
+class ShapleyValuesDependenceContainer(PlotContainer):
+    info = {
+        'name': "Shapley Values Dependence",
+        'plotType': "ShapleyValuesDependence",
+        'plotCategory': "Dataset Level",
+        'requiredParams': ["model", "variable"]
+    }
+    options_category = 'DatasetShapleyValues'
+    options = {}
+    def _fit(self, model, variable):
+        if variable.variable not in model.variables:
+            raise Exception('Variable is not a column of explainer')
+        is_numeric = is_numeric_dtype(model.explainer.data[variable.variable])
+        if is_numeric and variable.levels is None:
+            self.plot_component = 'LinearShapleyDependence'
+        else:
+            self.plot_component = 'CategoricalShapleyDependence'
+        resource = self.arena.resource_manager.get_resource('DatasetShapleyValues', {'model': model})
+        try:
+            data, progress, is_done = resource.get_result()
+            if data.get('result') is None:
+                resource.wait_for_update()
+                data, progress, is_done = resource.get_result()
+        except Exception as e:
+            self.set_message(str(e))
+            return
+        self.is_done = is_done
+        self.progress = progress
+        result = data.get('result')
+        result = result[result['variable_name'] == variable.variable]
+        stats = result.groupby(['variable_value', 'row']).agg({'contribution': ['mean', 'min', 'max']}).contribution
+        if is_numeric and variable.levels is not None:
+            stats = stats.sort_index()
+        self.data = {
+            'x': [str(x[0]) for x in stats.index],
+            'mean': stats['mean'].values.tolist(),
+            'min': stats['min'].values.tolist(),
+            'max': stats['max'].values.tolist(),
+            'variable': variable.variable
+        }
