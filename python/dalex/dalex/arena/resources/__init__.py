3477d60b623aa244f8147c6e7558f8100b68a18f
Piotr Piątyszek <piotrp@wektor.xyz>
2021-03-06 22:37:35 +0000

    Add resource for shapley values on the dataset

--- python/dalex/dalex/arena/resources/__init__.py
+++ python/dalex/dalex/arena/resources/__init__.py
@@ -1,5 +1,7 @@
 from ._shapley_values_resource import ShapleyValuesResource
+from ._dataset_shapley_values_resource import DatasetShapleyValuesResource
 
 __all__ = [
-    'ShapleyValuesResource'
+    'ShapleyValuesResource',
+    'DatasetShapleyValuesResource'
 ]
