9e8aff895fc7e86c52e1a0ab4712dcbf2c178c0f
Piotr Piątyszek <piotrp@wektor.xyz>
2021-05-04 11:40:58 +0000

    Update docs

--- python/dalex/dalex/arena/_plot_container.py
+++ python/dalex/dalex/arena/_plot_container.py
@@ -3,6 +3,40 @@ from .params import ModelParam, DatasetParam, VariableParam, ObservationParam
 
 
 class PlotContainer(OptionBase):
+    """
+    Class representing a chart.
+    
+    Parameters
+    ----------
+    arena : dalex.Arena
+        Instance of Arena.
+    cache : bool
+        If this object is allowed to use cache when requesting resources
+
+    Attributes
+    --------
+    arena : Arena
+        Instance of dalex.Arena
+    name : str
+        Display name of chart
+    plot_type : str
+        Identifier of chart type
+    plot_component : str
+        Identifier of Arena's component that should render this chart
+    plot_category : str
+        Name of category of chart
+    params : dict
+        Dictionary with required param types as keys and param labels as values.
+        This attribute is set when calling fit.
+    data : dict
+        Results of computations are placed there
+    progress : float
+        If progress is supprted, then value should be between [0,1]. For other situations -1 value must be set.
+        Progress of plot container is based of progress of used resources at the moment of calling fit method.
+        This value will not be updated.
+    use_cache : bool
+        If this object is allowed to use cache when requesting resources
+    """
     def __init__(self, arena, cache=True):
         super().__init__(arena)
         info = self.__class__.info
@@ -17,6 +51,18 @@ class PlotContainer(OptionBase):
         # If plot class is allowed to use cache when requesting resources
         self.use_cache = cache
     def fit(self, params):
+        """Function computes plots data for given params
+
+        Parameters
+        -----------
+        params : dict
+            Keys of this dict are params types (model, observation, variable, dataset)
+            and values are corresponding params values (class Param).
+
+        Returns
+        --------
+        PlotContainer object
+        """
         required_params = {}
         for p in self.__class__.info.get('requiredParams'):
             self.check_param(p, params.get(p))
@@ -26,6 +72,13 @@ class PlotContainer(OptionBase):
         self._fit(**required_params)
         return self
     def serialize(self):
+        """Saves important attributes of PlotContainer into a dict.
+        Returned dict is meant to be directly put into Arena data file.
+
+        Returns
+        --------
+        dict
+        """
         return {
             'name': self.name,
             'plotType': self.plot_type,
@@ -37,12 +90,34 @@ class PlotContainer(OptionBase):
             'isDone': True if self.is_done is None else self.is_done
         }
     def set_message(self, msg, msg_type='info'):
+        """Changes plot component to message and sets data with provided message
+
+        Parameters
+        -----------
+        msg : str
+            Text of message
+        msg_type : str
+            Type of message. One of ['info', 'error']
+        """
         if msg_type != 'info' and msg_type != 'error':
             raise Exception('Invalid message type')
         self.plot_component = 'Message'
         self.data = {'message': msg, 'type': msg_type}
 
     def check_param(self, param_type, value):
+        """Function validates param values as param of given type
+
+        Parameters
+        -----------
+        param_type : str
+            One of ['model', 'variable', 'observation', 'dataset'].
+        value : Object
+            Function checks if this object have correct class.
+
+        Returns
+        --------
+        None if value is correct. Else raises exception
+        """
         correct_class = {'model': ModelParam, 'variable': VariableParam, 'observation': ObservationParam, 'dataset': DatasetParam}.get(param_type)
         if not isinstance(value, correct_class):
             raise Exception('Invalid param ' + str(param_type))
