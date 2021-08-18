c01276221bddc0dac4a1fc0c73f4a432a6dd4bba
krzyzinskim <mateusz_krzyzinski@wp.pl>
2021-08-18 12:01:00 +0200

    remove unused tooltip

--- python/dalex/dalex/aspect/_model_aspect_importance/plot.py
+++ python/dalex/dalex/aspect/_model_aspect_importance/plot.py
@@ -41,22 +41,3 @@ def tooltip_text_aspect(row, rounding_function, digits):
         + "Variables:<br>"
         + "<br>".join(row.variables_names)
     )
-
-
-def tooltip_text(row, rounding_function, digits):
-    if row.difference > 0:
-        key_word = "+"
-    else:
-        key_word = "-"
-    return (
-        "Model: "
-        + str(row.label)
-        + " loss after<br>aspect: "
-        + str(row.aspect_name)
-        + " is permuted: "
-        + str(rounding_function(row.dropout_loss, digits))
-        + "<br>"
-        + "Drop-out loss change: "
-        + key_word
-        + str(rounding_function(np.abs(row.difference), digits))
-    )
