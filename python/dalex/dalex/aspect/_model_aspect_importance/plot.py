076184f267aa00701bcfd2d6eaa404c239b12783
krzyzinskim <mateusz_krzyzinski@wp.pl>
2021-08-27 00:37:26 +0200

    move single tooltip

--- python/dalex/dalex/aspect/_model_aspect_importance/plot.py
+++ python/dalex/dalex/aspect/_model_aspect_importance/plot.py
@@ -1,25 +1,7 @@
 import numpy as np
 
 
-def tooltip_text_single(row, rounding_function, digits):
-    if row.difference > 0:
-        key_word = "+"
-    else:
-        key_word = "-"
-    return (
-        "Drop-out loss: "
-        + str(rounding_function(row.dropout_loss, digits))
-        + "<br>"
-        + "Drop-out loss change: "
-        + key_word
-        + str(rounding_function(np.abs(row.difference), digits))
-        + "<br>"
-        + "Variable: "
-        + str(row.aspect_name)
-    )
-
-
-def tooltip_text_aspect(row, rounding_function, digits):
+def tooltip_text(row, rounding_function, digits):
     if row.difference > 0:
         dropout_loss_change_string = "+" + str(
             rounding_function(row.difference, digits)
